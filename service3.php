98b875bc03312454b87b37a00c87baa502b78d39
Juan773 <juanquezada@upeu.edu.pe>
2020-05-26 01:46:29 -0500

    php

--- service3.php
+++ service3.php
@@ -3,14 +3,17 @@ include('lib/nusoap.php');
 $client = new nusoap_client('https://cvnet.cpd.ua.es/servicioweb/publicos/pub_gestdocente.asmx?wsdl','wsdl');
 $err = $client->getError();
 if ($err) {	echo 'Error en Constructor' . $err ; }
-$param = array('plengua' => 'C','pcurso'=> '2011-12','pcodcen'=>'B142','pcodest'=>'G');
+$param = array('plengua' => 'C','pcurso'=> '2011-12','pcoddep'=>'B142','pcodest'=>'');
+$param2 = array('plengua' => 'C','pcurso'=> '2010-11','pcodasi'=>'9244','porden'=>'T');
+$param3 = array('plengua' => 'v','pcurso'=> '2011-12');
 
-
-$result1 = $client->call('wsagrupaciones', $param);
-
+$result1 = $client->call('wsasidepto', $param);
+$result2 = $client->call('wshorarios', $param2);
+$result3 = $client->call('wstitulosuni ', $param3);
 
 print_r($result1);
-
+print_r($result2);
+print_r($result3);
 
 ?>
 
